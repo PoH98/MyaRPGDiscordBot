038a6b2e73aa277f332569496924b6d7f4616e29
PoH98 <shunyuan98@gmail.com>
2022-04-12 09:13:13 +0800

    fixed shop minor logic

--- MyaDiscordBot/ButtonEvent/ShopElementHandler.cs
+++ MyaDiscordBot/ButtonEvent/ShopElementHandler.cs
@@ -18,7 +18,7 @@ namespace MyaDiscordBot.ButtonEvent
         {
             var element = message.Data.CustomId.Replace("shopEle-", "");
             var builder = new ComponentBuilder();
-            builder.WithButton("武器", "shopType-" + element + "-weapon").WithButton("護甲", "shopType-" + element + "-amor").WithButton("戒指", "shopType-" + element + "-ring").WithButton("頸鏈", "shopType-" + element + "-necklece").WithButton("鞋", "shopType-" + element + "-shoes").WithButton("護盾", "shopType-" + element + "-shield").WithButton("消耗品", "shopType-" + element + "-none");
+            builder.WithButton("武器", "shopType-" + element + "-weapon").WithButton("護甲", "shopType-" + element + "-amor").WithButton("戒指", "shopType-" + element + "-ring").WithButton("頸鏈", "shopType-" + element + "-necklece").WithButton("鞋", "shopType-" + element + "-shoes").WithButton("護盾", "shopType-" + element + "-shield");
             return message.RespondAsync("OK~咁請問你想買咩類型的野？", ephemeral: true, components: builder.Build());
         }
     }
--- MyaDiscordBot/Commands/Shop.cs
+++ MyaDiscordBot/Commands/Shop.cs
@@ -14,7 +14,7 @@ namespace MyaDiscordBot.Commands
         public async Task Handler(SocketSlashCommand command, DiscordSocketClient client)
         {
             var builder = new ComponentBuilder();
-            builder.WithButton("光屬性裝備", "shopEle-light").WithButton("暗屬性裝備", "shopEle-dark").WithButton("風屬性裝備", "shopEle-wind").WithButton("火屬性裝備", "shopEle-fire").WithButton("地屬性裝備", "shopEle-earth").WithButton("水屬性裝備", "shopEle-water").WithButton("道具", "shopEle-none");
+            builder.WithButton("光屬性裝備", "shopEle-light").WithButton("暗屬性裝備", "shopEle-dark").WithButton("風屬性裝備", "shopEle-wind").WithButton("火屬性裝備", "shopEle-fire").WithButton("地屬性裝備", "shopEle-earth").WithButton("水屬性裝備", "shopEle-water").WithButton("道具/消耗品", "shopType-none-none");
             await command.RespondAsync("你搖一搖院友卡，小貓精靈就馬上受到你的召喚，已經出現係你面前！≧◉ᴥ◉≦", components: builder.Build(), ephemeral: true);
         }
     }
